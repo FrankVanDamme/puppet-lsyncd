b5087fb80d8f0b614c2ca867e610c52d1206ab63
Frank Van Damme <frank.van.damme@vub.be>
2024-11-14 15:55:07 +0100

    legacy facts

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,12 +4,13 @@ class lsyncd::params {
   $max_user_watches = undef
   $service_name = 'lsyncd'
   $package_name = 'lsyncd'
-  case $::osfamily {
+
+  case ${facts[os][family]} {
     'Redhat': {
       $config_file = "/etc/lsyncd.conf"
     }
     'Debian': {
-      case $::operatingsystem {
+      case ${facts[os][name]} {
         'Ubuntu': {
           $config_file = "${config_dir}/lsyncd.conf.lua"
         }
@@ -17,12 +18,12 @@ class lsyncd::params {
           $config_file = "${config_dir}/lsyncd.conf.lua"
         }
         default: {
-          fail("Unsupported platform: ${module_name} currently doesn't support ${::osfamily} or ${::operatingsystem}")
+          fail("Unsupported platform: ${module_name} currently doesn't support ${facts[os][family]} or ${facts[os][name]}")
         }
       }
     }
     default: {
-      fail("Unsupported platform: ${module_name} currently doesn't support ${::osfamily} or ${::operatingsystem}")
+      fail("Unsupported platform: ${module_name} currently doesn't support ${facts[os][family]} or ${facts[os][name]}")
     }
   }
 }
